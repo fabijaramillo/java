7c3b86dbd9f8d4ffd186fc9d1be92cd861b4d6f7
Fabian Jaramillo <fabianjaramillo@unimayor.edu.co>
2025-09-22 11:46:28 -0500

    Organizar carpetas: dia01 y Switch

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,11 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        System.out.println("*** Operadores Aritmeticos ***");
+        int a = 5, b = 3, resultado = 0;
+        // Suma +
+        resultado = a + b;
+        System.out.println("resultado suma = " + resultado);
+        //resta
+
+    }
+}
---
+++ SistemaDescuentosVIP.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class SistemaDescuentosVIP {
+    public static void main(String[] args) {
+        System.out.println(" Sistema de Descuentos VIP");
+
+            final var No_PRODUCTOS_DESCUENTO = 10;
+            var consola =  new Scanner("");
+    }
+}
